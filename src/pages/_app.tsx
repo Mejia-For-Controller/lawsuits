229a2def750604d3795beccc1ae0b1727243dab0
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2022-09-12 10:23:26 -0700

    gtm

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,6 @@
 import { AppProps } from 'next/app';
+import { useEffect } from 'react';
+import TagManager from 'react-gtm-module'
 
 import '@/styles/globals.css';
 // !STARTERCONF This is for demo purposes, remove @/styles/colors.css import immediately
@@ -9,7 +11,15 @@ import '@/styles/colors.css';
  * ? `Layout` component is called in every page using `np` snippets. If you have consistent layout across all page, you can add it here too
  */
 
+const tagManagerArgs = {
+  gtmId: 'GTM-MQG62S5'
+}
+
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    TagManager.initialize(tagManagerArgs)
+  })
+
   return <Component {...pageProps} />;
 }
 
